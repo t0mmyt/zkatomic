1033bae5eb18df71f6b60669c9824e768d754970
Tom Taylor <tom+github@log2n.uk>
2017-09-08 11:43:00 +0100

    Downgraded curator libs to support ZK 3.4

--- src/main/java/uk/tommyt/zkatomic/Cluster.java
+++ src/main/java/uk/tommyt/zkatomic/Cluster.java
@@ -3,14 +3,15 @@ package uk.tommyt.zkatomic;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
+//import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryNTimes;
 
 public class Cluster {
     RetryPolicy retryPolicy;
     CuratorFramework client;
 
     public Cluster(String zkConnString) {
-        retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        retryPolicy = new RetryNTimes(0, 0);
         client = CuratorFrameworkFactory.newClient(zkConnString, retryPolicy);
         client.start();
     }
